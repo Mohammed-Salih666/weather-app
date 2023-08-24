f175d8fb3549b5ec65f1db70e9c9a5a3cf36dbc5
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-08-24 23:27:45 +0300

    Make the fetchWeather function return only the object with the desired data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+//https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=london
+
+
+
+
 let weatherData; 
 const fetchWeather = async (location) => {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=${location}`); 
 
-    weatherData = await response.json();  
-    // console.log(weatherData); 
-    return await weatherData; 
+    weatherData = await response.json();
+
+    return {
+        country: weatherData.location.country,
+        region: weatherData.location.region,
+        time: weatherData.location.localtime,
+        temperature_c: weatherData.current.temp_c, 
+        temperature_f: weatherData.current.temp_f,
+        feelslike_c: weatherData.current.feelslike_c,
+        feelslike_f: weatherData.current.feelslike_f, 
+        humidity: weatherData.current.humidity, 
+        wind_speed: weatherData.current.wind_kph,
+        icon: weatherData.current.condition.icon
+    
+    }
     
 }
+
+console.log(fetchWeather("Berlin"));
+
