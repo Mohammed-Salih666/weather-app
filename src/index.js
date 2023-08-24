363606dd598ad7ba5eb17e8b8357922b04fb82a3
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-08-24 23:07:03 +0300

    Finalize the fetchWeather() function so that it returns the desired data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-//https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=london
-
 let weatherData; 
 const fetchWeather = async (location) => {
-    const weatherInfo = await fetch(`https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=${location}`).then(data => {return data}); 
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=${location}`); 
 
-    weatherData = await weatherInfo.json();  
-    console.log(weatherData);
+    weatherData = await response.json();  
+    // console.log(weatherData); 
+    return await weatherData; 
+    
 }
-
