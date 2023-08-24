840834008d00c7d51f4bcb0946de7d8afea1822d
Mohammed-Salih666 <mohammedsaleh0218@gmail.com>
2023-08-24 18:26:07 +0300

    Edit the fetchWeather function so that it assigns the current weather data to the global variable weatherData;

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 //https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=london
 
+let weatherData; 
 const fetchWeather = async (location) => {
-    const weatherInfo = await fetch(`https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=${location}`); 
+    const weatherInfo = await fetch(`https://api.weatherapi.com/v1/current.json?key=d699d2e252dc44d0a3a151414232308&q=${location}`).then(data => {return data}); 
 
-    return weatherInfo.json();
+    weatherData = await weatherInfo.json();  
+    console.log(weatherData);
 }
+
